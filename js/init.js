e026aa30268e058fe663c64a722bf95621ae363c
salinaspk14 <94633775+salinaspk14@users.noreply.github.com>
2021-11-18 10:49:43 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBYsPHpdYXy6jvmrRt-vNh5COOePT4GmGE",
+  authDomain: "rfbdwp801.firebaseapp.com",
+  projectId: "rfbdwp801",
+  storageBucket: "rfbdwp801.appspot.com",
+  messagingSenderId: "335302255418",
+  appId: "1:335302255418:web:15427d0d702517c426a603",
+  measurementId: "G-13RB54RPGY"
+    });
